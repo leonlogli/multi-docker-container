c6ca51989f3037a7c3f53321bef41a4de2717dcd
Léon Logli <leonlogli@gmail.com>
2023-04-14 11:15:24 +0200

    Add volume and run containers

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Docker "Docker multiple containers" Example
+# Docker "Multi containers" Example
 
-Docker example that shows how to setup and run docker multi containers.
+Docker example that shows how networks work.
 
 ## Prerequisites
 
@@ -8,4 +8,45 @@ Before you get started, you need to have the following tools installed:
 
 - Docker
 
+## Build the frontend and backend images
+
+Navigate to the root directory of the cloned repository:
+
+Build the backend image:
+
+```bash
+docker build -t backend ./backend/.
+```
+
+Build the backend image:
+
+```bash
+docker build -t frontend ./frontend/.
+```
+
 ## Running the Containers
+
+Create docker network:
+
+```bash
+docker network create multi-containers
+```
+
+Run mongo db container in the created network `multi-containers`:
+
+```bash
+docker run -d --name multi-containers-db -v multi-containers-db:/data/db --rm  --network multi-containers mongo
+```
+
+Run the backend container in the same network:
+
+```bash
+docker run -d -p 8089:80 --rm --network multi-containers --name backend backend
+```
+
+Run the frontend container (no need to specify the network because the front app run in browser, not in docker directly who resolve network):
+
+```bash
+# the front app needs a pseudo terminal `it` to keep running
+docker run -d -p 8090:3000 --rm -it --name frontend frontend
+```
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import GoalInput from './components/goals/GoalInput';
-import CourseGoals from './components/goals/CourseGoals';
-import ErrorAlert from './components/UI/ErrorAlert';
+import GoalInput from './components/goals/GoalInput'
+import CourseGoals from './components/goals/CourseGoals'
+import ErrorAlert from './components/UI/ErrorAlert'
 
 function App() {
-  const [loadedGoals, setLoadedGoals] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [loadedGoals, setLoadedGoals] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(function () {
     async function fetchData() {
-      setIsLoading(true);
+      setIsLoading(true)
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch('http://localhost:8080/goals')
 
-        const resData = await response.json();
+        const resData = await response.json()
 
         if (!response.ok) {
-          throw new Error(resData.message || 'Fetching the goals failed.');
+          throw new Error(resData.message || 'Fetching the goals failed.')
         }
 
-        setLoadedGoals(resData.goals);
+        setLoadedGoals(resData.goals)
       } catch (err) {
         setError(
           err.message ||
             'Fetching goals failed - the server responsed with an error.'
-        );
+        )
       }
-      setIsLoading(false);
+      setIsLoading(false)
     }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   async function addGoalHandler(goalText) {
-    setIsLoading(true);
+    setIsLoading(true)
 
     try {
-      const response = await fetch('http://localhost/goals', {
+      const response = await fetch('http://localhost:8080/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
         }),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+          'Content-Type': 'application/json',
+        },
+      })
 
-      const resData = await response.json();
+      const resData = await response.json()
 
       if (!response.ok) {
-        throw new Error(resData.message || 'Adding the goal failed.');
+        throw new Error(resData.message || 'Adding the goal failed.')
       }
 
       setLoadedGoals((prevGoals) => {
@@ -62,43 +62,43 @@ function App() {
             text: goalText,
           },
           ...prevGoals,
-        ];
-        return updatedGoals;
-      });
+        ]
+        return updatedGoals
+      })
     } catch (err) {
       setError(
         err.message ||
           'Adding a goal failed - the server responsed with an error.'
-      );
+      )
     }
-    setIsLoading(false);
+    setIsLoading(false)
   }
 
   async function deleteGoalHandler(goalId) {
-    setIsLoading(true);
+    setIsLoading(true)
 
     try {
-      const response = await fetch('http://localhost/goals/' + goalId, {
+      const response = await fetch('http://localhost:8080/goals/' + goalId, {
         method: 'DELETE',
-      });
+      })
 
-      const resData = await response.json();
+      const resData = await response.json()
 
       if (!response.ok) {
-        throw new Error(resData.message || 'Deleting the goal failed.');
+        throw new Error(resData.message || 'Deleting the goal failed.')
       }
 
       setLoadedGoals((prevGoals) => {
-        const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
-        return updatedGoals;
-      });
+        const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId)
+        return updatedGoals
+      })
     } catch (err) {
       setError(
         err.message ||
           'Deleting the goal failed - the server responsed with an error.'
-      );
+      )
     }
-    setIsLoading(false);
+    setIsLoading(false)
   }
 
   return (
@@ -109,7 +109,7 @@ function App() {
         <CourseGoals goals={loadedGoals} onDeleteGoal={deleteGoalHandler} />
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
